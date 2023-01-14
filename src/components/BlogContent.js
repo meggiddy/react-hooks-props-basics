1622d32228a547a4f9b57a1c94e94859d5f13458
meggidy <mimamitchelle@gmail.com>
2023-01-14 11:10:01 +0300

    Completed Assignments

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -6,7 +6,7 @@ function BlogContent(props) {
   } else {
     return (
       <div id="blog-content">
-        <h1>{props.articleText}</h1>;
+        <h1>{props.articleText}</h1>
         <p>{props.minutesToRead} minutes to read</p>
       </div>
     );
